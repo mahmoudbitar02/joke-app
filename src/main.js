564e3f664330fe5ce0921c9549910a36cfb279bb
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2026-04-04 23:39:43 +0200

    work on delete btn

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const jokeButton = document.querySelector(".current-joke__actions--new");
 const saveButton = document.querySelector(".current-joke__actions--save");
 const jokeElement = document.querySelector(".current-joke__text");
 
-document.addEventListener("DOMContentLoaded", () => renderSavedJokes);
+document.addEventListener("DOMContentLoaded", renderSavedJokes);
 renderJoke();
 
 jokeButton.addEventListener("click", () => {
@@ -35,7 +35,7 @@ saveButton.addEventListener("click", () => {
     alert("Dieser Witz ist bereits gespeichert!");
     return;
   }
-  savedJokes.push(currentJoke);
+  savedJokes.unshift(currentJoke);
   localStorage.setItem("savedJokes", JSON.stringify(savedJokes));
   renderSavedJokes();
 
@@ -45,40 +45,37 @@ saveButton.addEventListener("click", () => {
 function renderSavedJokes() {
   const savedJokes = getSavedJokes();
   const savedJokesContainer = document.querySelector(".saved-joke__container");
-
-  savedJokesContainer.innerHTML = "";
+  let html = "";
 
   if (savedJokes.length === 0) {
     savedJokesContainer.innerHTML = "<em class='saved-joke__empty'>Keine gespeicherten Witze.</em>";
     return;
   } else {
     savedJokes.forEach((joke, indx) => {
-      const jokeItem = document.createElement("div");
-      jokeItem.classList.add("saved-joke__body");
-
-      jokeItem.innerHTML = `
-             
-              <p class="saved-joke__text">${joke}</p>
-              <button class="saved-joke__delete">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  class="saved-joke__remove-icon"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="m3 3 1.664 1.664M21 21l-1.5-1.5m-5.485-1.242L12 17.25 4.5 21V8.742m.164-4.078a2.15 2.15 0 0 1 1.743-1.342 48.507 48.507 0 0 1 11.186 0c1.1.128 1.907 1.077 1.907 2.185V19.5M4.664 4.664 19.5 19.5"
-                  />
-                </svg>
-              </button>
+      html += `
+        <div class="saved-joke__body" data-index="${indx}">
+          <p class="saved-joke__text">${joke}</p>
+          <button class="saved-joke__delete">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="saved-joke__remove-icon"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m3 3 1.664 1.664M21 21l-1.5-1.5m-5.485-1.242L12 17.25 4.5 21V8.742m.164-4.078a2.15 2.15 0 0 1 1.743-1.342 48.507 48.507 0 0 1 11.186 0c1.1.128 1.907 1.077 1.907 2.185V19.5M4.664 4.664 19.5 19.5"
+              />
+            </svg>
+          </button>
+        </div>
             
     `;
 
-      savedJokesContainer.appendChild(jokeItem);
+      savedJokesContainer.innerHTML = html;
     });
   }
 }
